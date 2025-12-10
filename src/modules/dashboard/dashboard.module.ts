991eaf60c0d84dbf301d1b839e61ad9b65b62905
ALE-OFI <air5795@gmail.com>
2025-12-10 15:32:02 -0400

    creacion de dashboard para admin y mejora de datos de empresa

--- src/modules/dashboard/dashboard.module.ts
+++ src/modules/dashboard/dashboard.module.ts
@@ -6,16 +6,12 @@ import { DashboardService } from './dashboard.service';
 // Importar solo las entidades, NO los módulos completos para evitar dependencias circulares
 import { PlanillasAporte } from '../planillas_aportes/entities/planillas_aporte.entity';
 import { Empresa } from '../empresas/entities/empresa.entity';
-import { PagoAporte } from '../pagos-aportes/entities/pagos-aporte.entity';
+import { SolicitudesReembolso } from '../reembolsos-incapacidades/solicitudes_reembolso/entities/solicitudes_reembolso.entity';
 
 @Module({
   imports: [
     // Solo registrar las entidades que necesita el dashboard
-    TypeOrmModule.forFeature([
-      PlanillasAporte,
-      Empresa,
-      PagoAporte,
-    ]),
+    TypeOrmModule.forFeature([PlanillasAporte, Empresa, SolicitudesReembolso]),
   ],
   controllers: [DashboardController],
   providers: [DashboardService],
